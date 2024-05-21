9be66ee16c8a4b70e0929b2ad17c0342f7220754
balagowshik <balagowshikmd77@gmail.com>
2024-05-22 02:02:33 +0530

    fix:cors policy test

--- components/Feed.js
+++ components/Feed.js
@@ -34,7 +34,6 @@ const Feed = () => {
                 alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
                 src={url}
                 ar
-                poster="shared-assets/models/NeilArmstrong.webp"
                 shadow-intensity="1"
                 camera-controls
                 touch-action="pan-y"
